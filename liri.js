7e2d2018e929b062dd7c84e59ca6375d2ed960b1
Toney Higgins <toneyhiggins2@gmail.com>
2019-09-25 23:22:39 -0400

    adding movie data

--- liri.js
+++ liri.js
@@ -15,10 +15,10 @@ if (selectAction === "concert-this") {
 Search.concertThis(userSearch);
 }
 else if (selectAction === "spotify-this-song") {
-
+Search.spotifySong(userSearch);
 }
 else if (selectAction === "movie-this") {
-
+Search.movieThis(userSearch);
 }
 else if (selectAction === "do-what-it-says") {
 
@@ -87,5 +87,34 @@ var Search = function() {
   });
 
     };
+
+    this.movieThis = function(movie) {
+        var URL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy"
+    
+        axios.get(URL).then(function(response) {
+          // Place the response.data into a variable, jsonData.
+          var jsonData = response.data;
+    
+          // showData ends up being the string containing the show data we will print to the console
+          var movieData = [
+            "Title: " + jsonData.Title,
+            "Year: " + jsonData.Year,
+            "IMDB Rating: " + jsonData.Rating[0].Source,
+            "Rotten Tomatoes Rating: " + jsonData.Rating[1].Source,
+            "Country: " + jsonData.Country,
+            "Language: " + jsonData.Language,
+            "Plot: " + jsonData.Plot,
+            "Actors: " + jsonData.Actors
+          ].join("\n\n");
+    
+          // Append showData and the divider to log.txt, print showData to the console
+          fs.appendFile("log.txt", movieData + divider, function(err) {
+            if (err) throw err;
+            console.log(showData);
+          });
+  
+          console.log(showData);
+        });
+      };
   };
 
